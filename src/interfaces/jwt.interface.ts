78f20e05521549d513c32aebbf7d42dd93ac8ee0
KBJ <ideasdeveloperskbj@gmail.com>
2020-09-25 21:44:12 -0500

    Backend de usuarios
    Desarrollo basico de los servicios para los usuarios,
    * login
    * Registro
    * Crud

--- src/interfaces/jwt.interface.ts
+++
@@ -1,4 +0,0 @@
-import { IUser } from './user.interface';
-export interface IJwt {
-    user: IUser;
-}
--- src/resolvers/mutation/index.ts
+++
@@ -1,8 +0,0 @@
-import GMR from 'graphql-merge-resolvers';
-import resolversUserMutation from './user';
-
-const mutationResolvers = GMR.merge([
-    resolversUserMutation
-]);
-
-export default mutationResolvers;
